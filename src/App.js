8d6d815d572d0cb1f9c8b750d4fd7102be11dfda
lerara <chanova.lerara@gmail.com>
2023-05-11 12:29:23 +0300

    created sign up, the form input, button components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navigation from './routes/navigation/navigation.component';
 import Shop from './routes/shop/shop.component';
 import SignIn from './routes/sign-in/sign-in.component';
 
+
 const App = () => {
   return (
     <Routes>
